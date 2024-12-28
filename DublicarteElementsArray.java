cf4b9c3a424614718f783c33346e094abc4f8e30
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2024-12-28 10:52:53 +0530

    Add files via upload

---
+++ DublicarteElementsArray.java
@@ -0,0 +1,47 @@
+package com.jspiders.arrays_dsa;
+
+import java.util.Scanner;
+
+public class DublicarteElementsArray
+{
+	static void frequencyElement(int arr[])
+	{
+		
+		int dub=0;
+		boolean b[] = new boolean[arr.length];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(b[i]==false)
+			{
+				for(int j=0;j<arr.length;j++)
+				{
+					if(arr[i] == arr[j])
+					{
+						dub++;
+						b[j]=true;
+					} 
+				}
+				if(dub>1)//for Dublicating Elements 
+					System.out.println(arr[i] );
+				
+//				lr++;
+				dub=0;
+			}	
+				
+		}
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Size of the Array");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		frequencyElement(arr);
+	}
+}
