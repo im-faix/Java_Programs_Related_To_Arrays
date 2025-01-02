c627ee2dd41430ab70433010fd054db9c1d3ed03
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2025-01-02 10:28:00 +0530

    "Pair of Elements to sum of given by Programmer "

--- SumPair.java
+++ SumPair.java
@@ -1,28 +1,20 @@
-package com.jspiders.arrays_dsa;
-
 import java.util.Scanner;
 
 public class SumPair 
 {
-	static void sumPair(int arr[])
+	static void sumPair(int arr[],int sum)
 	{
-		int sum=6;
-//		boolean b[] = new boolean[arr.length];
+
 		for(int i=0;i<arr.length;i++)
 		{
-//			if(b[i]==false)
-//			{
 			     for(int j=i+1;j<arr.length;j++)
 			    {
 				   if(arr[i]+arr[j] == sum)
 				   {
 					    System.out.println(arr[i] + " "+arr[j]);
-//					    b[j]=true;
 				    }
-//			}
+				}
 		}
-		}
-		
 	}
 	public static void main(String[] args) 
 	{
@@ -30,8 +22,6 @@ public class SumPair
 		System.out.println("Enter the size of the array ");
 		int n = sc.nextInt();
 		int[] arr = new int[n];
-//		int max=0;
-//		int max=arr[0];
 		for(int i=0;i<arr.length;i++)
 		{
 			arr[i]=sc.nextInt();
@@ -42,7 +32,7 @@ public class SumPair
 			System.out.println(arr[i]);
 		}
 		System.out.println();
-		sumPair(arr);
+		sumPair(arr,sum);
 		
 	}
 }
