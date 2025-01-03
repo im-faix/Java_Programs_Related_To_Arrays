f309b163061f8512a4b715074ebd015f960ee78e
Mohammed Faizan <156441568+im-faix@users.noreply.github.com>
2025-01-03 10:31:03 +0530

    Add files via upload

---
+++ FrequencyArray.java
@@ -0,0 +1,47 @@
+package com.jspiders.arrays_dsa;
+
+import java.util.Scanner;
+
+public class FrequencyArray
+{
+	static void frequencyCount(int arr[])
+	{
+//		int count =0;
+//		int lr =arr[0];
+		boolean b[] = new boolean[arr.length];
+		for(int i=0;i<arr.length;i++)
+		{
+			if(b[i]==false)
+			{
+				int count=0;
+				for(int j=0;j<arr.length;j++)
+				{
+					if(arr[i] == arr[j])
+					{
+						count++;
+						b[j]=true;
+					} 			
+				}
+//				lr++;
+				
+				System.out.println(arr[i] +" "+ (count));
+				}
+		
+		}
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Size of the Array");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+//		System.out.println(frequencyCount(arr));
+		frequencyCount(arr);
+	}
+}
